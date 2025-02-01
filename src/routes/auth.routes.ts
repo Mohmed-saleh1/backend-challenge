6d60bf85680ed1f1aef569c7cb56f0b6a77430b4
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2025-02-01 15:54:47 +0200

    feat: implement validation middlewares

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,10 +1,14 @@
 import express from "express";
 import AuthController from "../controllers/auth.controller";
+import {
+  validateLogin,
+  validateRegister,
+} from "../utils/validations/auth.validations";
 
 const router = express.Router();
 
-router.post("/register", AuthController.register);
+router.post("/register", validateRegister, AuthController.register);
 
-router.post("/login", AuthController.login);
+router.post("/login", validateLogin, AuthController.login);
 
 export default router;
---
+++ src/utils/validations/auth.validations.ts
@@ -0,0 +1,35 @@
+import { body, validationResult } from "express-validator";
+import { Request, Response, NextFunction } from "express";
+
+// Validation middleware
+export const validateRegister = [
+  body("name").notEmpty().withMessage("Name is required"),
+  body("email").isEmail().withMessage("Invalid email address"),
+  body("password")
+    .isLength({ min: 6 })
+    .withMessage("Password must be at least 6 characters long"),
+
+  // Check for validation errors
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
+
+// Validation middleware for login
+export const validateLogin = [
+  body("email").isEmail().withMessage("Invalid email address"),
+  body("password").notEmpty().withMessage("Password is required"),
+
+  // Check for validation errors
+  (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
+];
