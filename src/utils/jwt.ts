cc863752366da88c07368e9dd83caebe861071fd
mohmed-saleh1 <mohamed.s.kamaal@gmail.com>
2025-02-03 13:32:08 +0200

    feat: add singelton design pattern

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,25 @@
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 class JWT {
-  private secret: string = process.env.JWT_SECRET || "secret";
+  private static instance: JWT;
+  private secret: string;
+
+  private constructor() {
+    if (!process.env.JWT_SECRET) {
+      throw new Error("JWT_SECRET is not defined in environment variables.");
+    }
+    this.secret = process.env.JWT_SECRET;
+  }
+
+  public static getInstance(): JWT {
+    if (!JWT.instance) {
+      JWT.instance = new JWT();
+    }
+    return JWT.instance;
+  }
 
   generateToken(payload: any): string {
     return jwt.sign(payload, this.secret, { expiresIn: "1h" });
@@ -11,16 +27,16 @@ class JWT {
 
   verifyToken(token: string): any {
     try {
-      console.log("the secreate is ", this.secret);
-
       return jwt.verify(token, this.secret);
     } catch (err) {
       if (err instanceof jwt.TokenExpiredError) {
         throw new Error("Token has expired.");
+      } else if (err instanceof jwt.JsonWebTokenError) {
+        throw new Error("Invalid token.");
       }
-      throw new Error("Invalid token.");
+      throw new Error("Token verification failed.");
     }
   }
 }
 
-export default new JWT();
+export default JWT.getInstance();
